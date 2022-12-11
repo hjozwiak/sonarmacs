a19af8bde111a857a989a09c98ed673ea2aa4a72
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-12-11 18:59:08 -0500

    User initialization.
    Finish adding the path to the early initialization.

--- early-init.el
+++ early-init.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2022  Hunter Jozwiak
 
 ;; Author: Hunter Jozwiak <hunter.t.joz@gmail.com>
-;; Keywords: 
+; Keywords: initialization
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -27,11 +27,34 @@
 (setq gc-cons-threshold (* 51000 10000))
 
 ;; Load the newest compiled files.
-(setq load-prefer-newer 'noninteractive')
+(setq load-prefer-newer 'noninteractive)
 
 ;; The load path for the user's configuration.
-(defvar sonar-configuration-path
-  ()
+(defvar sonarmacs-configuration-path
+  (cond
+   ((featurep 'chemacs)
+    (if (getenv "SONARMACS_HOME")
+        (expand-file-name (getenv "SONARMACS_HOME"))
+      (expand-file-name "sonarmacs" user-emacs-directory)))
+   ;; Do they have the variable in their environment
+   ((getenv "SONARMACS_HOME") (expand-file-name (getenv "SONARMACS_HOME")))
+   ;; Check and see if we have the XDG_CONFIG_HOME environment variable set
+   ((or (getenv "XDG_CONFIG_HOME") (file-exists-p "~/.config/sonarmacs"))
+    (if (getenv "XDG_CONFIG_HOME")
+        (expand-file-name "sonarmacs" (getenv "XDG_CONFIG_HOME"))
+      (expand-file-name "~/.config/sonarmacs")))
+    ((getenv "HOME") (expand-file-name ".sonarmacs" (getenv "HOME"))))
   "The place where user configuration is to be stored.")
-(provide 'early-init)
+
+;; Make the directory in the event that it doens't exist.
+(unless (file-exists-p sonarmacs-configuration-path)
+  (mkdir sonarmacs-configuration-path t))
+;; Tack it onto the load path now.
+(add-to-list 'load-path (expand-file-name sonarmacs-configuration-path))
+
+
 ;;; early-init.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
