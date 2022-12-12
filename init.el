f44399c992417e85d63a82204028f210d85da062
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-12-11 21:44:20 -0500

    Further initialization work.
    * Make MELPA the sole source for packages.
    * Define a useful customize macro for bulk setting custom variables.
    * Codify speechd-el initialization.

---
+++ init.el
@@ -0,0 +1,57 @@
+;;; init.el --- Initialize Emacs.                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Hunter Jozwiak
+
+;; Author: Hunter Jozwiak <hunter.t.joz@gmail.com>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; The core initialization system for GNU Emacs.
+;; Process the initial setup of the package system.
+;; Install setup.
+;; Install speechd-el.
+;; Hand off to loading modules.
+
+;;; Code:
+;; Load in some libraries
+;; For sequences.
+(require 'seq)
+;; The package system.
+(require 'package)
+(package-initialize)
+;; Add melpa to the package archives.
+(customize-set-variable 'package-archives '(melpa . "https://melpa.org/packages/"))
+
+;; Check and see if we need to refresh the contents.
+(unless (seq-empty-p package-archive-contents)
+  (message "The package system is empty: initializing.")
+  (package-refresh-contents))
+
+;; Setup setup.el
+(unless (package-installed-p 'setup)
+  (package-install 'setup))
+
+;; Handy macro for bulk defining customization forms.
+(defmacro csetq (&rest args)
+  "A macro like setq, but works with customize options."
+  `(setup (:options ,@args)))
+
+;; speechd-el
+(setup :package speechd-el
+       (speechd-speak))
+
+;;; init.el ends here
