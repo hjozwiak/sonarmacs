6e95c91ad4292fc9759bd0efd1636cff30454bca
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-12-12 18:33:04 -0500

    Whoops.

--- elpa/speechd-el-20220608.1422/speechd-el-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "speechd-el" "20220608.1422" "Client to speech synthesizers and Braille displays." 'nil :commit "7e30c439729d5635ddd341ad5ab16f832a4619ea" :authors
-  '(("Milan Zamazal" . "pdm@zamazal.org"))
-  :maintainer
-  '("Milan Zamazal" . "pdm@zamazal.org"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
