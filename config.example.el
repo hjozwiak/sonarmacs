0d7e151c0ca87972e58845ff9aabcad5e101c7eb
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-12-18 13:36:00 -0500

    Write an example configuration file.
    It's tiny but the gist should be there.

---
+++ config.example.el
@@ -0,0 +1,13 @@
+;; Example configuration for Sonarmacs.
+
+;; You can load modules like this:
+;; (modload foo)
+;; And pass further use-package arguments along.
+;; Similarly, you can set customization variables with the setc macro, which is aliased to general-setq.
+;; Tell Emacs who you are.
+(setc user-full-name "The Dude."
+      user-mail-address "thedude@earthlink.net")
+
+;; Load in some modules.
+(modload sonarmacs-evil)
+;; Enjoy!
