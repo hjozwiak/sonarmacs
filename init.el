7b4003d1f05b7e58ff8bfbc7ef8b901a5054de26
Hunter Jozwiak <hunter.t.joz@gmail.com>
2023-01-17 13:05:15 -0500

    Some definers for setup.
    Try to remove brltty from the list.

--- init.el
+++ init.el
@@ -8,9 +8,38 @@
   (package-install 'setup))
 (require 'setup)
 
+(setup-define :after-init
+  (lambda (&rest body)
+    `(if after-init-time (progn ,@body) (add-hook 'after-init-hook (lambda () ,@body)))
+  :documentation "Run the specified body after Emacs has finished initializing."
+  :signature '(body ...))
+
+(setup-define :after-gui
+  (lambda (&rest body)
+    `(if (and (not (daemonp)) (display-graphic-p))
+	 (progn ,@body)
+       (add-hook 'server-after-make-frame-hook (lambda ()
+						 (when (display-graphic-p)
+						   ,@body
+						   t)))))
+  :documentation "Run body after the first graphical frame is created."
+  :signature '(BODY ...))
+
+(setup-define :after-tty
+  (lambda (&rest body)
+    `(if (and (not (daemonp)) (not (display-graphic-p)))
+	 (progn ,@body)
+       (add-hook 'server-after-make-frame-hook (lambda ()
+						 (unless (display-graphic-p)
+						   ,@body
+						   t)))))
+  :documentation "Run body after the first TTY frame is created."
+  :signature '(body ...))
+
 (setup
     (:package speechd-el)
   (:option
+   (remove speechd-out-active-drivers) 'brltty
    speechd-speak-whole-line t
    speechd-speak-echo nil
    speechd-speak-read-command-keys nil
@@ -21,6 +50,7 @@
 
 (setup
     (:package no-littering)
+  (:require no-littering)
 (:option
   auto-save-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))
 custom-file (no-littering-expand-etc-file-name "custom.el"))))
