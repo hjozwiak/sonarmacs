818fe9b59d61db04582814d138fff1cd92270c31
Hunter Jozwiak <hunter.t.joz@gmail.com>
2023-01-17 10:47:29 -0500

    Go to a literate configuration.

--- init.el
+++ init.el
@@ -1,10 +1,26 @@
 ;; -*- lexical-binding: t; -*-
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
 (customize-set-variable 'package-archive-priorities '(("melpa" . 10) ("gnu" . 9) ("nongnu" . 8)))
-(package-refresh-contents)
-(unless (package-installed-p 'setup) (package-install 'setup))
-(setup (:package speechd-el) (speechd-speak))
-(setup (:package no-littering)
+
+(unless (package-installed-p 'setup)
+  (package-install 'setup))
+(require 'setup)
+
+(setup
+    (:package speechd-el)
+  (:option
+   speechd-speak-whole-line t
+   speechd-speak-echo nil
+   speechd-speak-read-command-keys nil
+   speechd-speak-voices '((nil
+			   (rate . 100)
+			   (output-module . "espeak-ng"))))
+  (speechd-speak))
+
+(setup
+    (:package no-littering)
 (:option
-  auto-save-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
+  auto-save-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))
+custom-file (no-littering-expand-etc-file-name "custom.el"))))
