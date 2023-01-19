e56ba1de01601aa12cc30178febf348daf108aaa
Hunter Jozwiak <hunter.t.joz@gmail.com>
2023-01-19 05:11:05 -0500

    Migration to use-package.
    It will be built in in future Emacsn and we can port over the existing modules into the org file that much easier.

--- init.el
+++ init.el
@@ -4,51 +4,21 @@
 
 (customize-set-variable 'package-archive-priorities '(("melpa" . 10) ("gnu" . 9) ("nongnu" . 8)))
 
-(unless (package-installed-p 'setup)
-  (package-install 'setup))
-(require 'setup)
+(unless (package-installed-p 'use-package)
+  (package-install ''use-package))
+(require 'use-package)
 
-(defmacro setc (&rest things)
-  `(setup (:option ,@things)))
-
-(setup-define :after-init
-  (lambda (&rest body)
-    `(if after-init-time (progn ,@body) (add-hook 'after-init-hook (lambda () ,@body)))
-  :documentation "Run the specified body after Emacs has finished initializing."
-  :signature '(body ...))
-
-(setup-define :after-gui
-  (lambda (&rest body)
-    `(if (and (not (daemonp)) (display-graphic-p))
-         (progn ,@body)
-       (add-hook 'server-after-make-frame-hook (lambda ()
-                                                 (when (display-graphic-p)
-                                                   ,@body
-                                                   t)))))
-  :documentation "Run body after the first graphical frame is created."
-  :signature '(BODY ...))
-
-(setup-define :after-tty
-  (lambda (&rest body)
-    `(if (and (not (daemonp)) (not (display-graphic-p)))
-         (progn ,@body)
-       (add-hook 'server-after-make-frame-hook (lambda ()
-                                                 (unless (display-graphic-p)
-                                                   ,@body
-                                                   t)))))
-  :documentation "Run body after the first TTY frame is created."
-  :signature '(body ...))
-
-(setup
-    (:package speechd-el)
-  (:option
-   speechd-speak-whole-line t
-   speechd-speak-echo nil
-   speechd-speak-read-command-keys nil
-   speechd-voices '((nil
-                           (rate . 100)
-                           (output-module . "espeak-ng"))))
-  (speechd-speak))
+(use-package speechd-el
+  :ensure t
+  :custom
+   (speechd-speak-whole-line t)
+   (speechd-speak-echo nil)
+   (speechd-speak-read-command-keys nil)
+   (speechd-voices '((nil
+			   (rate . 100)
+			   (output-module . "espeak-ng"))))
+:init
+(speechd-speak))
 
 (defun retrieve-speechd-function (thing)
   "Retrieve the function that is defined by speechd-el.
@@ -59,30 +29,66 @@ Usually it is the form of speechd-speak-read-<thing>"
 	   when (fboundp s)
 	   return s))
 
-(cl-loop for ent in '("paragraph" "sentence" "word" "character" "sexp")
-	 for doc = (format "Advise the functions that move by %s to report the new %s after movement." ent ent)
-	 for setup-name = (intern (concat ":" ent "-feedback"))
-	 for speechd-command = (retrieve-speechd-function ent)
-	 when (fboundp speechd-command) do
-	 (eval
-	  `(setup-define ,setup-name
-	     (lambda (&rest funcs)
-	       `(speechd-speak--command-feedback ,funcs after (funcall speechd-command)))
-	     :documentation ,doc
-	     :signature '(command ...))))
+(use-package no-littering
+  :ensure t
+  :demand t
+  :custom
+  (auto-save-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+  (custom-file (no-littering-expand-etc-file-name "custom.el"))))
 
-(setup-define :line-feedback
-  (lambda (&rest funcs)
-    `(speechd-speak-command-feedback ,funcs after (speechd-speak-read-line (not speechd-speak-whole-line))))
-  :documentation "Advise the functions that move by lines to report the new line after execution."
-  :signature '(COMMAND ...))
+(use-package which-key
+  :ensure t
+  :custom
+  (which-key-idle-delay 1)
+  :init
+  (which-key-mode))
 
-(setup
-    (:package no-littering)
-  (:require no-littering)
-(:option
-  auto-save-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))
-custom-file (no-littering-expand-etc-file-name "custom.el"))))
+(use-package general
+  :ensure t
+  :config
+  (general-auto-unbind-keys)
+  (general-evil-setup t)
+  ;; Create the leader definer
+  (general-create-definer mapleader
+    :prefix "SPC"
+    :global-prefix "C-SPC"
+    :states '(normal visual insert emacs))
+  (mapleader
+   "b" '(:ignore t :which-key "Buffer operations")
+   "bn" '(next-buffer :which-key "Go to the next buffer.")
+   "bp" '(previous-buffer :which-key "Go to the previous buffer.")
+   "bb" '(switch-to-buffer :which-key "Switch to a buffer.")
+   "bi" '(ibuffer :which-key "Interactive buffer management through the Ibuffer interface.")
+   "br" '(revert-buffer :which-key "Revert the buffer to how it is on disk.")
+   ;; Customization
+   "c" '(:ignore t :which-key "Customization interface.")
+   "cc" '(customize :which-key "Open the customization index.")
+   "cf" '(customize-face :which-key "Customize a face.")
+   "cg" '(customize-group :which-key "Customize a specific group.")
+   "cv" '(customize-variable :which-key "Customize a variable in the custom interface.")
+   "cV" '(customize-set-variable :which-key "Expert level variable setting, sans interface.")
+   "f" '(:ignore t :which-key "File operations.")
+   "fs" '(save-buffer :which-key "Save your currently opened file.")
+   "ff" '(find-file :which-key "Find a file.")
+   ;; Frame operations
+   "F" '(:ignore t "Frames.")
+   "Fd" '(delete-frame :which-key "Delete this frame.")
+   "Fo" '(other-frame :which-key "Go to another frame.")
+   "h" '(:ignore t :which-key "Help")
+   "hd" '(:ignore t :which-key "Describe parts of Emacs.")
+   "hdb" '(describe-bindings :which-key "Describe the keybindings that are in effect right now.")
+   "hdB" '(general-describe-keybindings :which-key "Get a list of the key bindings that are in effect via General.")
+   "hdf" '(describe-function :which-key "Describe a function.")
+   "hdF" '(describe-face :which-key "Describe a face.")
+   "hdk" '(describe-key :which-key "Describe what a key is bound to.")
+   "hdp" '(describe-package :which-key "Describe a package.")
+   ;; Info manuals
+   "hi" '(:ignore t :which-key "Info")
+   "hia" '(info-apropos :which-key "Search the info database.")
+   "hii" '(info-index :which-key "Open the info index.")
+   "him" '(info-display-manual :which-key "Open a specific info manual.")))
+;; Aliases
+(defalias 'setc #'general-setq "A convenience alias for setting customizable variables.")
 
 (setc user-full-name "Hunter Jozwiak"
       user-mail-address "hunter.t.joz@gmail.com"
